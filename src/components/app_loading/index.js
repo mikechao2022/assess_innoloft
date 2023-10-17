9dc5892a65810c73b08b2a36a23e400f0572e8b4
mikechao2022 <mike.chao2022@gmail.com>
2023-10-17 13:48:40 -0400

    feat: add app loading component

--- src/components/app_loading/index.js
+++ src/components/app_loading/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Logo from "../../assets/logo.svg";
 
 const AppLoading = () => {
   return (
-    <div className="text-5xl w-full h-screen flex justify-center items-center">
-      Loading...
+    <div className="text-2xl w-full h-screen flex flex-col justify-center items-center">
+      <img className="w-48" src={Logo} alt="Innoloft" />
+      <p className="mt-8">Loading...</p>
     </div>
   );
 };
